227b3c70dfe29170b08a3d4d14284f3672707b50
Nadezhda Tarasova <taras19nadezdin@gmail.com>
2021-04-29 14:42:53 +0300

    .

--- HotelBookingSystem/Controllers/HotelsSearchController.cs
+++ HotelBookingSystem/Controllers/HotelsSearchController.cs
@@ -8,6 +8,7 @@ using AutoMapper;
 using Business.Interfaces;
 using Business.Models;
 using Business.ViewModels;
+using Microsoft.Extensions.Logging;
 
 namespace WebAPI.Controllers
 {
@@ -17,16 +18,22 @@ namespace WebAPI.Controllers
     {
         private readonly IHotelsSearchService _hotelsSearchService;
         private readonly IMapper _mapper;
+        private readonly ILogger<HotelsSearchController> _logger;
 
-        public HotelsSearchController(IHotelsSearchService hotelsSearchService, IMapper mapper)
+        public HotelsSearchController(IHotelsSearchService hotelsSearchService, IMapper mapper, ILogger<HotelsSearchController> logger)
         {
             _hotelsSearchService = hotelsSearchService;
             _mapper = mapper;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Search(HotelSearchRequestModel searchModel)
         {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError($"There is some error in the {searchModel}");
+            }
 
             // List<HotelEntity>.select(x=> _mapper.Map<Hotel>(x)) -> List<Hotel>
             return Ok(_mapper.Map<Hotel>(_hotelsSearchService.Find(new HotelsSearchModel())));
---
+++ HotelBookingSystem/Services/TokenService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.ModelBinding;
+using Business.ViewModels;
+using Business.ViewModels.Authorization;
+using Business.ViewModels.Authorization.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI.Interfaces;
+using TokenOptions = Business.ViewModels.Authorization.TokenOptions;
+
+namespace WebAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+
+        private readonly TokenOptions _tokenOptions;
+
+        public TokenService(IOptions<TokenOptions> tokens)
+        {
+            _tokenOptions = tokens.Value;
+        } 
+        private string GenerateJWT()
+        {
+            // Create token key
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenOptions.Key));
+            SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Set token expiration
+            DateTime startTime = DateTime.UtcNow;
+            DateTime expiryTime = startTime.AddMinutes(15);
+
+            // Generate the token
+            JwtSecurityToken token =
+                new JwtSecurityToken(
+                    "Microsoft.Security.Bearer",
+                    "Microsoft.Security.Bearer",
+                    null,
+                    notBefore: startTime,
+                    expires: expiryTime,
+                    signingCredentials: credentials);
+
+            string result = new JwtSecurityTokenHandler().WriteToken(token); // for creating and validating Json Web Tokens
+
+            return result;
+        }
+
+        public TokenOptions GenerateToken()
+        {
+            TokenOptions token = new TokenOptions();
+            token.Token = GenerateJWT();
+            return token;
+        }
+    }
+}
