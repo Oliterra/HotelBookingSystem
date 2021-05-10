83d7ab30d4728202ef07620079d0af0291f7eee5
Nadezhda Tarasova <taras19nadezdin@gmail.com>
2021-05-10 11:11:56 +0300

    Implemented CRUD with Redux
    Enabled CORS in Web API
    Consume API web methods from React
    Real-Time Form Validation
    React Form Design with Material UI

---
+++ hotelbookingsystem.ui/src/components/HotelForm.js
@@ -0,0 +1,208 @@
+import React, { useState, useEffect } from "react"; 
+import { Grid, TextField, withStyles, FormControl, InputLabel, Select, MenuItem, Button, FormHelperText } from "@material-ui/core"; 
+import useForm from "./useForm.js"; 
+import { connect } from "react-redux"; 
+import * as actions from "../actions/hotel"; 
+import { useToasts } from "react-toast-notifications"; 
+
+
+const styles = theme => ({ 
+    root: { 
+        '& .MuiTextField-root': { 
+            margin: theme.spacing(1), 
+            minWidth: 230, 
+        } 
+    }, 
+
+    formControl: { 
+        margin: theme.spacing(1), 
+        minWidth: 230, 
+    }, 
+    smMargin: { 
+        margin: theme.spacing(1) 
+    } 
+}) 
+
+const initialFieldValues = { 
+    name: '', 
+    starsCount: '', 
+    country: '', 
+    city: '', 
+    priceFrom: '', 
+    priceTo: '', 
+    address: '' 
+} 
+
+const HotelForm = ({ classes, ...props }) => { 
+    const { addToast } = useToasts() 
+
+    const validate = (fieldValues = values) => { 
+
+        let temp = { ...errors } 
+
+        if ('name' in fieldValues) 
+            temp.fullName = fieldValues.fullName ? "" : "This field is required." 
+        if ('starsCount' in fieldValues) 
+            temp.mobile = fieldValues.mobile ? "" : "This field is required." 
+        if ('country' in fieldValues) 
+            temp.bloodGroup = fieldValues.bloodGroup ? "" : "This field is required." 
+        if ('city' in fieldValues) 
+            temp.bloodGroup = fieldValues.bloodGroup ? "" : "This field is required." 
+        if ('address' in fieldValues) 
+            temp.bloodGroup = fieldValues.bloodGroup ? "" : "This field is required." 
+        setErrors({ 
+            ...temp 
+        }) 
+
+        if (fieldValues == values) 
+            return Object.values(temp).every(x => x == "") 
+    } 
+
+    const { 
+        values, 
+        setValues, 
+        errors, 
+        setErrors, 
+        handleInputChange, 
+        resetForm 
+    } = useForm(initialFieldValues, validate, props.setCurrentId) 
+
+    const inputLabel = React.useRef(null); 
+
+    const [labelWidth, setLabelWidth] = React.useState(0); 
+
+    React.useEffect(() => { 
+        setLabelWidth(inputLabel.current.offsetWidth); 
+    }, []); 
+
+    const handleSubmit = e => { 
+        e.preventDefault() 
+        if (validate()) { 
+            const onSuccess = () => { 
+                resetForm() 
+                addToast("Submitted successfully", { appearance: 'success' }) 
+            } 
+            if (props.currentId == 0) 
+                props.createHotel(values, onSuccess) 
+            else 
+               props.updateHotel(props.currentId, values, onSuccess) 
+        } 
+    } 
+    useEffect(() => { 
+        if (props.currentId != 0) { 
+            setValues({ 
+
+                ...props.hotelList.find(x => x.id == props.currentId) 
+            }) 
+            setErrors({}) 
+        } 
+    }, [props.currentId]) 
+
+    return ( 
+        <form autoComplete="off" noValidate className={classes.root} onSubmit={handleSubmit}> 
+            <Grid container> 
+                <Grid item xs={6}> 
+                    <TextField 
+                        name="name" 
+                        variant="outlined" 
+                        label="Name" 
+                        value={values.name} 
+                        onChange={handleInputChange} 
+                        {...(errors.name && { error: true, helperText: errors.name })} 
+                    /> 
+                    <FormControl variant="outlined" 
+                        className={classes.formControl} 
+                        {...(errors.starsCount && { error: true })} 
+                    > 
+                        <InputLabel ref={inputLabel}>Count of stars</InputLabel> 
+                        <Select 
+                            name="starsCount" 
+                            value={values.starsCount} 
+                            onChange={handleInputChange} 
+                            labelWidth={labelWidth} 
+                        > 
+                            <MenuItem value="">Select number of stars</MenuItem> 
+                            <MenuItem value="1">1</MenuItem> 
+                            <MenuItem value="2">2</MenuItem> 
+                            <MenuItem value="3">3</MenuItem> 
+                            <MenuItem value="4">4</MenuItem> 
+                            <MenuItem value="5">5</MenuItem> 
+                        </Select> 
+                        {errors.starsCount && <FormHelperText>{errors.starsCount}</FormHelperText>} 
+                    </FormControl> 
+                </Grid> 
+                <Grid item xs={6}> 
+                    <TextField 
+                        name="country" 
+                        variant="outlined" 
+                        label="Country" 
+                        value={values.country} 
+                        onChange={handleInputChange} 
+                        {...(errors.country && { error: true, helperText: errors.country })} 
+                    /> 
+
+                    <TextField 
+                        name="city" 
+                        variant="outlined" 
+                        label="City" 
+                        value={values.city} 
+                        onChange={handleInputChange} 
+                        {...(errors.city && { error: true, helperText: errors.city })} 
+                    /> 
+
+                    <TextField 
+                        name="priceFrom" 
+                        variant="outlined" 
+                        label="Price from" 
+                        value={values.priceFrom} 
+                        onChange={handleInputChange} 
+                    /> 
+                    <TextField 
+                        name="priceTo" 
+                        variant="outlined" 
+                        label="Price to" 
+                        value={values.priceTo} 
+                        onChange={handleInputChange} 
+                    /> 
+                    <TextField 
+                        name="address" 
+                        variant="outlined" 
+                        label="Address" 
+                        value={values.address} 
+                        onChange={handleInputChange} 
+                        {...(errors.address && { error: true, helperText: errors.address })} 
+                    /> 
+                    <div> 
+                        <Button 
+                            variant="contained" 
+                            color="primary" 
+                            type="submit" 
+                            className={classes.smMargin} 
+                        > 
+                            Submit 
+                        </Button> 
+
+                        <Button 
+                            variant="contained" 
+                            className={classes.smMargin} 
+                            onClick={resetForm} 
+                        > 
+                            Reset 
+                        </Button> 
+                    </div> 
+                </Grid> 
+            </Grid> 
+        </form> 
+    ); 
+} 
+
+const mapStateToProps = state => ({ 
+    hotelList: state.hotel.list 
+}) 
+
+const mapActionToProps = { 
+    createHotel: actions.create, 
+    updateHotel: actions.update 
+} 
+
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(HotelForm)); 
---
+++ hotelbookingsystem.ui/src/components/Hotels.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react"; 
+import { connect } from "react-redux"; 
+import * as actions from "../actions/hotel"; 
+import { Grid, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, withStyles, ButtonGroup, Button } from "@material-ui/core"; 
+import HotelForm from "./HotelForm"; 
+import EditIcon from "@material-ui/icons/Edit"; 
+import DeleteIcon from "@material-ui/icons/Delete"; 
+import { useToasts } from "react-toast-notifications"; 
+
+
+const styles = theme => ({ 
+    root: { 
+        "& .MuiTableCell-head": { 
+            fontSize: "1.25rem"
+        } 
+    }, 
+
+    paper: { 
+        margin: theme.spacing(2), 
+        padding: theme.spacing(2) 
+    } 
+}) 
+
+const Hotels = ({ classes, ...props }) => { 
+    const [currentId, setCurrentId] = useState(0) 
+
+    useEffect(() => { 
+        props.fetchAllHotels() 
+    }, [props]) 
+
+    const { addToast } = useToasts() 
+
+    const onDelete = id => { 
+      if (window.confirm('Are you sure to delete this record?')) 
+            props.deleteHotel(id,()=>addToast("Deleted successfully", { appearance: 'info' })) 
+    } 
+
+    return ( 
+        <Paper className={classes.paper} elevation={3}> 
+            <Grid container> 
+                <Grid item xs={6}> 
+                    <HotelForm {...({ currentId, setCurrentId })} /> 
+                </Grid> 
+                <Grid item xs={6}> 
+                    <TableContainer> 
+                        <Table> 
+                            <TableHead className={classes.root}> 
+                                <TableRow> 
+                                    <TableCell>Name</TableCell> 
+                                    <TableCell>Number os stars</TableCell> 
+                                    <TableCell>Country</TableCell> 
+                                    <TableCell>City</TableCell> 
+                                    <TableCell>Price From</TableCell> 
+                                    <TableCell>Price To</TableCell> 
+                                    <TableCell>Address</TableCell> 
+                                </TableRow> 
+                            </TableHead> 
+                            <TableBody> 
+                                { 
+                                    props.hotelList.map((record, index) => { 
+                                        return (<TableRow key={index} hover> 
+                                            <TableCell>{record.name}</TableCell> 
+                                            <TableCell>{record.starsCount}</TableCell> 
+                                            <TableCell>{record.country}</TableCell> 
+                                            <TableCell>{record.city}</TableCell> 
+                                            <TableCell>{record.priceFrom}</TableCell> 
+                                            <TableCell>{record.priceTo}</TableCell> 
+                                            <TableCell>{record.address}</TableCell> 
+                                            <TableCell> 
+                                                <ButtonGroup variant="text"> 
+                                                    <Button><EditIcon color="primary" 
+                                                        onClick={() => { setCurrentId(record.id) }} /></Button> 
+                                                    <Button><DeleteIcon color="secondary" 
+                                                        onClick={() => onDelete(record.id)} /></Button> 
+                                                </ButtonGroup> 
+                                            </TableCell> 
+                                        </TableRow>) 
+                                    }) 
+                                } 
+                            </TableBody> 
+                        </Table> 
+                   </TableContainer> 
+                </Grid> 
+            </Grid> 
+        </Paper> 
+    ); 
+} 
+const mapStateToProps = state => ({ 
+    hotelList: state.hotel.list 
+}) 
+
+const mapActionToProps = { 
+    fetchAllHotels: actions.fetchAll, 
+    deleteHotel: actions.Delete 
+} 
+
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(Hotels)); 
