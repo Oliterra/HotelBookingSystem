263e617f2a0d2da48557031a0e95bcb6801d99d4
Nadezhda Tarasova <taras19nadezdin@gmail.com>
2021-05-13 12:27:20 +0300

    header and footer sketches were added

--- hotelbookingsystem.ui/src/App.js
+++ hotelbookingsystem.ui/src/App.js
@@ -5,19 +5,34 @@ import { Provider } from "react-redux";
 import Hotels from './components/Hotels'; 
 import { Container } from "@material-ui/core"; 
 import { ToastProvider } from "react-toast-notifications";
-import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import HotelsForUsers from './components/HotelsForUsers';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() { 
   return ( 
-    <Provider store={store}> 
-    <ToastProvider autoDismiss={true}> 
-      <Container maxWidth="lg"> 
-        <HotelsForUsers /> 
-      </Container> 
-    </ToastProvider> 
-  </Provider> 
+    <div>
+
+      <div>
+        <Header />
+      </div>
+
+      <div>
+         <Provider store={store}> 
+           <ToastProvider autoDismiss={true}> 
+             <Container maxWidth="lg"> 
+               <HotelsForUsers /> 
+             </Container> 
+           </ToastProvider> 
+         </Provider> 
+      </div>
+      
+      <div>
+        <Footer />
+      </div>
+
+    </div>
   ); 
 } 
 
